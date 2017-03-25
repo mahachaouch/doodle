cd2c1700a0db9ee4a12873a35f82f8c5a1260d36
HugoPasero <hugo.pasero@gmail.com>
2017-03-25 19:46:25 +0100

    fonctions réponses et authentifications

--- index.js
+++ index.js
@@ -14,51 +14,64 @@ app.post('/id/:id/date/:date/heure/:heure',function(req,res){
     res.json(doodle.ajouterCreneauEvenement(req.params.id,req.params.date,req.params.heure));
 });
 
-app.get('/evt/:id/date/:date/heure/:heure', function (req, res) {   
+app.get('/evt/:id/date/:date/heure/:heure', function (req, res) {
     res.json(doodle.retirerCreneauEvenement(req.params.id,req.params.date,req.params.heure));
 });
 
-app.get('/evt/:id', function (req, res) {   
+app.get('/evt/:id', function (req, res) {
     res.json(doodle.supprimerEvenement(req.params.id));
 });
 
 app.get('/oui/:id/date/:date/heure/:heure', function (req, res){
+  if (doodle.auth == null) {
+      console.log('il faut vous connecter');
+  }
+  else {
     res.json(doodle.repondreCreneauEvenementOui(req.params.id,req.params.date,req.params.heure));
+  }
 });
 
 app.get('/non/:id/date/:date/heure/:heure', function (req, res){
+  if (doodle.auth == null) {
+      console.log('il faut vous connecter');
+  }
+  else {
     res.json(doodle.repondreCreneauEvenementNon(req.params.id,req.params.date,req.params.heure));
+  }
 });
 
 app.get('/evt', function(req, res){
     if (doodle.auth == null) {
-        res.json();
+        console.log('il faut vous connecter');
     }
     else {
         res.json(doodle.afficherEvt());
-    }    
+    }
 });
 
 app.post('/profil/:login/nom/:nom/prenom/:prenom', function(req, res){
     res.json(doodle.creerProfil(req.params.login, req.params.nom, req.params.prenom));
 });
 
+app.get('/connected/:login',function(req,res){
+    res.json(doodle.isConnected(req.params.login));
+});
+
 app.get('/auth/:login',function(req, res) {
     if (doodle.connexion(req.params.login) != null) {
         doodle.auth = req.params.login;
-        res.json(doodle.afficherEvt());
-        console.log('connecté en tant que '+ doodle.auth);
+        console.log('connecté en tant que '+ doodle.auth+' isConnected '+doodle.rechercheProfil(req.params.login).connecte);
+        res.json(doodle.rechercheProfil(req.params.login));
     }
     else {
-        console.log('erreur connexion, login incconu');
-        res.json(doodle.afficherEvt());
+        console.log('erreur connexion, login inconnu');
     }
 });
 
-app.get('/auth',function(req, res) {
+app.get('/logOut',function(req, res) {
     doodle.auth = null;
+    doodle.logOut();
     console.log('vous avez été deconnecté');
-    res.json(doodle.afficherEvt());
 });
 
 app.listen(8080,function(){
