3bb29ae86a61c839a51a548316985893a5cf8229
mahachaouch <maha-chaouch@hotmail.fr>
2017-03-26 15:10:32 +0200

    ajout des nouvelles pages

---
+++ app/scripts/controllers/creerProfil.js
@@ -0,0 +1,44 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module('doodleApp')
+  .controller('CreerProfilCtrl', function ($scope,$http) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    
+    $scope.profil={
+        login:"",
+        nom:"",
+       prenom:""
+    };
+    
+    $scope.login;
+    $scope.nom;
+    $scope.prenom;
+    $scope.creerProfil=function(){
+           $http({method: 'POST', url: '/profil/'+$scope.login+'/nom/'+$scope.nom+'/prenom/'+$scope.prenom}).then(function successCallback(response) {
+     // code si réussite
+      $scope.evtResponse=response.data;
+      console.log(response.data);
+     });
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  });
